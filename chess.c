5da33e1c3506964adb816173ce1a94852bf16ccd
Hayden Freeman <mail@haydfree.com>
2026-04-19 12:29:26 -0500

    moving is fully working (i think)

--- chess.c
+++ chess.c
@@ -7,44 +7,34 @@
 #define HEIGHT_BOARD WIDTH_BOARD
 #define NUM_COL 8
 #define NUM_ROW 8
-#define NUM_PC 2
-#define NUM_PT 6
+#define NUM_COLORS 2
+#define NUM_TYPES 6
 #define PX_PER_SQ (WIDTH_BOARD / NUM_COL)
 #define PATH_TX "assets"
 
-typedef enum piece_color {pc_NONE=-1,pc_W,pc_B} piece_color;
-typedef enum piece_type {pt_NONE=-1,pt_P,pt_N,pt_B,pt_R,pt_Q,pt_K} piece_type;
-typedef enum piece_captured {pcap_FALSE, pcap_TRUE} piece_captured;
-typedef struct square {
-	piece_color pc;
-	piece_type pt;
-} square;
-typedef struct board {square squares[NUM_ROW][NUM_COL];} board;
-typedef struct coord {int row, col;} coord;
-typedef struct move {
-	coord start, end;
-	piece_captured pcap;
-} move;
-typedef struct game {
-	move m[MAX_GAME_MOVES];
-	move pm[MAX_MOVE_MOVES];
-	board bd;
-	piece_color turn;
-	int score, fm, fg;
-	coord cb[2];
+typedef enum color_t {c_NONE=-1,c_W,c_B} color_t;
+typedef enum type_t {t_NONE=-1,t_P,t_N,t_B,t_R,t_Q,t_K} type_t;
+typedef enum cap_t {cap_FALSE, cap_TRUE} cap_t;
+typedef struct square_t {color_t color; type_t type;} square_t;
+typedef struct board_t {square_t squares[NUM_ROW][NUM_COL];} board_t;
+typedef struct coord_t {int row, col;} coord_t;
+typedef struct move_t {coord_t start, end;cap_t cap;} move_t;
+typedef struct texture_t {Texture2D pieces[NUM_COLORS][NUM_TYPES];Texture2D board;} texture_t;
+typedef struct game_t {
+	move_t moves[MAX_GAME_MOVES];
+	move_t pmoves[MAX_MOVE_MOVES];
+	board_t board;
+	color_t turn;
+	int score, flag_g;
+	coord_t sel[2];
 	size_t mc, pmc;
 	Vector2 mp;
-} game;
-typedef struct textures {
-	Texture2D pieces[NUM_PC][NUM_PT];
-	Texture2D board;
-} textures;
+} game_t;
+typedef void mvt_t(board_t*,color_t,move_t[MAX_MOVE_MOVES],size_t*);
 
-typedef void mv_vtable(board*,piece_color,move[MAX_MOVE_MOVES],size_t*);
-
-void board_init(board *b) {
-square initial_b[NUM_ROW][NUM_COL]={
-	{(square){pc_B,pt_R},(square){pc_B,pt_N},(square){pc_B,pt_B},(square){pc_B,pt_Q},(square){pc_B,pt_K},(square){pc_B,pt_B},(square){pc_B,pt_N},(square){pc_B,pt_R}},{(square){pc_B,pt_P},(square){pc_B,pt_P},(square){pc_B,pt_P},(square){pc_B,pt_P},(square){pc_B,pt_P},(square){pc_B,pt_P},(square){pc_B,pt_P},(square){pc_B,pt_P}},{(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE}},{(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE}},{(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE}},{(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE},(square){pc_NONE,pt_NONE}},{(square){pc_W,pt_P},(square){pc_W,pt_P},(square){pc_W,pt_P},(square){pc_W,pt_P},(square){pc_W,pt_P},(square){pc_W,pt_P},(square){pc_W,pt_P},(square){pc_W,pt_P}},{(square){pc_W,pt_R},(square){pc_W,pt_N},(square){pc_W,pt_B},(square){pc_W,pt_Q},(square){pc_W,pt_K},(square){pc_W,pt_B},(square){pc_W,pt_N},(square){pc_W,pt_R}}};
+void board_init(board_t *b) {
+	square_t initial_b[NUM_ROW][NUM_COL]={
+	{(square_t){c_B,t_R},(square_t){c_B,t_N},(square_t){c_B,t_B},(square_t){c_B,t_Q},(square_t){c_B,t_K},(square_t){c_B,t_B},(square_t){c_B,t_N},(square_t){c_B,t_R}},{(square_t){c_B,t_P},(square_t){c_B,t_P},(square_t){c_B,t_P},(square_t){c_B,t_P},(square_t){c_B,t_P},(square_t){c_B,t_P},(square_t){c_B,t_P},(square_t){c_B,t_P}},{(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE}},{(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE}},{(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE}},{(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE},(square_t){c_NONE,t_NONE}},{(square_t){c_W,t_P},(square_t){c_W,t_P},(square_t){c_W,t_P},(square_t){c_W,t_P},(square_t){c_W,t_P},(square_t){c_W,t_P},(square_t){c_W,t_P},(square_t){c_W,t_P}},{(square_t){c_W,t_R},(square_t){c_W,t_N},(square_t){c_W,t_B},(square_t){c_W,t_Q},(square_t){c_W,t_K},(square_t){c_W,t_B},(square_t){c_W,t_N},(square_t){c_W,t_R}}};
 
 	for (int row=0;row<NUM_ROW;row++) {
 		for (int col=0;col<NUM_COL;col++) {
@@ -53,33 +43,33 @@ square initial_b[NUM_ROW][NUM_COL]={
 	}
 }
 
-void textures_init(textures *t) {
-	for (int pc=pc_W;pc<NUM_PC;pc++) {
-		for (int pt=pt_P;pt<NUM_PT;pt++) {
-			t->pieces[pc][pt]=LoadTexture(
-				TextFormat("%s/%d%d.png",PATH_TX,pc,pt));
+void textures_init(texture_t *t) {
+	for (int color=c_W;color<NUM_COLORS;color++) {
+		for (int type=t_P;type<NUM_TYPES;type++) {
+			t->pieces[color][type]=LoadTexture(
+				TextFormat("%s/%d%d.png",PATH_TX,color,type));
 		}
 	}
     	t->board = LoadTexture(TextFormat("%s/board.png",PATH_TX));
 }
 
-void textures_deinit(textures *t) {
-	for (int pc=pc_W;pc<NUM_PC;pc++) {
-		for (int pt=pt_P;pt<NUM_PT;pt++) {
-			UnloadTexture(t->pieces[pc][pt]);
+void textures_deinit(texture_t *t) {
+	for (int color=c_W;color<NUM_COLORS;color++) {
+		for (int type=t_P;type<NUM_TYPES;type++) {
+			UnloadTexture(t->pieces[color][type]);
 		}
 	}
 	UnloadTexture(t->board);
 }
 
-void px_to_coord(Vector2 *v, coord *c) {
-	coord new = {0};
+void px_to_coord(Vector2 *v, coord_t *c) {
+	coord_t new = {0};
 	new.row=v->y/PX_PER_SQ;
 	new.col=v->x/PX_PER_SQ;
 	*c=new;
 }
 
-void coord_to_px(coord *c, Vector2 *v, int centered) {
+void coord_to_px(coord_t *c, Vector2 *v, int centered) {
 	Vector2 new = {c->col*PX_PER_SQ,c->row*PX_PER_SQ};
 	if (centered) {
 		new.x+=PX_PER_SQ/2;
@@ -88,160 +78,184 @@ void coord_to_px(coord *c, Vector2 *v, int centered) {
 	*v=new;
 }
 
-void board_draw(board *b, textures *t) {
+void board_draw(board_t *b, texture_t *t) {
 	DrawTexture(t->board,0,0,WHITE);
 
 	for (int row=0;row<NUM_ROW;row++) {
 		for (int col=0;col<NUM_COL;col++) {
-			square *s=&b->squares[row][col];
-		        if (s->pc!=pc_NONE) {
-		        	coord c={row,col};
+			square_t *s=&b->squares[row][col];
+		        if (s->color!=c_NONE) {
+		        	coord_t c={row,col};
 			        Vector2 v={0};
 			        coord_to_px(&c,&v,0);
-		        	DrawTextureV(t->pieces[s->pc][s->pt], v, WHITE);
+		        	DrawTextureV(t->pieces[s->color][s->type], v, WHITE);
 		        }
 		}
 	}
 }
 
-void coord_add(coord *a, coord *b) {
-	coord new = {0};
+void coord_add(coord_t *a, coord_t *b) {
+	coord_t new = {0};
 	new.row = a->row+b->row;
 	new.col = a->col+b->col;
 	*a=new;
 }
 
-int is_ob(coord *a) {
+int is_coord_equal(coord_t *a, coord_t *b) {
+	return a->row==b->row&&a->col==b->col;
+}
+
+int is_ob(coord_t *a) {
 	int ret = 0;
 	if (a->row>=NUM_ROW||a->row<0||a->col>=NUM_COL||a->col<0) ret=1;
 	return ret;
 }
 
-void flip_deltas(coord *d, size_t ds) {
+void flip_deltas(coord_t *d, size_t ds) {
 	for (size_t i=0;i<ds;i++) {
-		coord new={0};
+		coord_t new={0};
 		new.row=d[i].row*-1;
 		new.col=d[i].col;
 		d[i]=new;
 	}
 }
 
-void moves_gen_p(board *b, piece_color pc, move *pm, size_t *pmc) {
+void moves_p_gen(board_t *b, color_t color, move_t *pm, size_t *pmc) {
 	const size_t ds=4;
-	coord deltas[]={{1,-1},{1,0},{1,1},{2,0}};
-	if (pc==pc_W) flip_deltas(deltas,ds);
+	coord_t deltas[]={{1,-1},{1,0},{1,1},{2,0}};
+	if (color==c_W) flip_deltas(deltas,ds);
 
 	for (int row=0;row<NUM_ROW;row++) {
 		for (int col=0;col<NUM_COL;col++) {
-			square s=b->squares[row][col];
-			if (s.pc!=pc||s.pt!=pt_P) continue;
+			square_t s=b->squares[row][col];
+			if (s.color!=color||s.type!=t_P) continue;
 			for (size_t d=0;d<ds;d++) {
-				coord c={row,col};
-				piece_captured pcap = pcap_FALSE;
+				coord_t c={row,col};
+				cap_t cap = cap_FALSE;
 				coord_add(&c,&deltas[d]);
 				if (is_ob(&c)) continue;
-				if (b->squares[c.row][c.col].pc!=pc_NONE)
-					pcap = pcap_TRUE;
-				pm[(*pmc)++]= (move) {
-					.start=(coord){row,col},
+				if (b->squares[c.row][c.col].color!=c_NONE)
+					cap = cap_TRUE;
+				pm[(*pmc)++]= (move_t) {
+					.start=(coord_t){row,col},
 					.end=c,
-					.pcap=pcap
+					.cap=cap
 				};
 			}
 		}
 	}
 }
-void moves_gen_n(board *b, piece_color pc, move *pm, size_t *pmc) {
-	(void)b;(void)pc;(void)pm;(void)pmc;
+
+void moves_n_gen(board_t *b, color_t color, move_t *pm, size_t *pmc) {
+	(void)b;(void)color;(void)pm;(void)pmc;
 }
-void moves_gen_b(board *b, piece_color pc, move *pm, size_t *pmc) {
-	(void)b;(void)pc;(void)pm;(void)pmc;
+
+void moves_b_gen(board_t *b, color_t color, move_t *pm, size_t *pmc) {
+	(void)b;(void)color;(void)pm;(void)pmc;
 }
-void moves_gen_r(board *b, piece_color pc, move *pm, size_t *pmc) {
-	(void)b;(void)pc;(void)pm;(void)pmc;
+
+void moves_r_gen(board_t *b, color_t color, move_t *pm, size_t *pmc) {
+	(void)b;(void)color;(void)pm;(void)pmc;
 }
-void moves_gen_q(board *b, piece_color pc, move *pm, size_t *pmc) {
-	(void)b;(void)pc;(void)pm;(void)pmc;
+
+void moves_q_gen(board_t *b, color_t color, move_t *pm, size_t *pmc) {
+	(void)b;(void)color;(void)pm;(void)pmc;
 }
-void moves_gen_k(board *b, piece_color pc, move *pm, size_t *pmc) {
-	(void)b;(void)pc;(void)pm;(void)pmc;
+
+void moves_k_gen(board_t *b, color_t color, move_t *pm, size_t *pmc) {
+	(void)b;(void)color;(void)pm;(void)pmc;
 }
-void moves_gen_all(board *b,piece_color pc,move pm[MAX_MOVE_MOVES],
-	size_t *pmc,mv_vtable **mvt) {
+
+void moves_all_gen(board_t *b,color_t color,move_t pm[MAX_MOVE_MOVES],
+	size_t *pmc,mvt_t **mvt) {
 	for (int row=0;row<NUM_ROW;row++) {
 		for (int col=0;col<NUM_COL;col++) {
-			square *s=&b->squares[row][col];
-			if (s->pt!=pt_NONE&&s->pc==pc) 
-				mvt[s->pt](b,pc,pm,pmc);
+			square_t *s=&b->squares[row][col];
+			if (s->type!=t_NONE&&s->color==color) 
+				mvt[s->type](b,color,pm,pmc);
 		}
 	}
 }
 
-void clear_square(square *s) {
-	s->pc=pc_NONE;
-	s->pt=pt_NONE;
+void clear_square(square_t *s) {
+	s->color=c_NONE;
+	s->type=t_NONE;
 }
 
-void move_make(board *b, coord cb[2]) {
-	square *start=&b->squares[cb[0].row][cb[0].col];
-	square *end=&b->squares[cb[1].row][cb[1].col];
+void move_make(board_t *b, coord_t sel[2]) {
+	square_t *start=&b->squares[sel[0].row][sel[0].col];
+	square_t *end=&b->squares[sel[1].row][sel[1].col];
 	*end=*start;
 	clear_square(start);
 }
 
-void moves_draw(move *moves, size_t s, coord *c) {
+void moves_draw(move_t *pmoves, size_t s, coord_t *c) {
 	for (size_t i=0;i<s;i++) {
-		if (moves[i].start.row!=c->row||moves[i].start.col!=c->col)
+		if (pmoves[i].start.row!=c->row||pmoves[i].start.col!=c->col)
 			continue;
 		Vector2 v={0};
-		coord_to_px(&moves[i].end,&v,1);
+		coord_to_px(&pmoves[i].end,&v,1);
 		DrawCircleV(v, 5.0, GRAY);
 	}
 }
 
-int is_click(Vector2 *mp) {
+int is_click(Vector2 *mp, coord_t *c) {
 	int ret = 0;
 	if (IsMouseButtonPressed(0)) {
 		*mp = GetMousePosition();
+		px_to_coord(mp,c);
 		ret = 1;
 	}
 	return ret;
 }
 
-int is_click_piece(board *b, Vector2 *mp, coord *c) {
+int is_click_piece(board_t *b, coord_t *c) {
 	int ret=0;
-	coord new={0};
-	px_to_coord(mp, &new);
-	if (b->squares[new.row][new.col].pc!=pc_NONE) {
-		ret=1;
-		*c=new;
-	}
+	if (b->squares[c->row][c->col].color!=c_NONE) ret=1;
 	return ret;
 }
 
-void clear_pm(move *pm, size_t *s) {
+void pmoves_clear(move_t *pmoves, size_t *s) {
 	for (size_t i=0;i<*s;i++) {
-		pm[i] = (move) {
-			.start=(coord){-1,-1},
-			.end=(coord){-1,-1},
-			.pcap=pcap_FALSE
+		pmoves[i] = (move_t) {
+			.start=(coord_t){-1,-1},
+			.end=(coord_t){-1,-1},
+			.cap=cap_FALSE
 		};
 	}
 	*s=0;
 }
 
-void cb_push(coord cb[2], coord *c) {
-	cb[0]=cb[1];
-	cb[1]=*c;
+void sel_clear(coord_t sel[2]) {
+	coord_t c={-1,-1};
+	sel[0]=c;
+	sel[1]=c;
 }
 
-int is_valid_move(move *moves, size_t s, coord cb[2]) {
+int is_sel_empty(coord_t sel[2]) {
+	int ret=0;
+	coord_t c={-1,-1};
+	if (is_coord_equal(&sel[0],&c)&&is_coord_equal(&sel[1],&c)) ret=1;
+	return ret;
+}
+
+int is_sel_full(coord_t sel[2]) {
+	int ret=0;
+	coord_t c={-1,-1};
+	if (!is_coord_equal(&sel[0],&c)&&!is_coord_equal(&sel[1],&c)) ret=1;
+	return ret;
+}
+
+void sel_push(coord_t sel[2], coord_t *c) {
+	sel[0]=sel[1];
+	sel[1]=*c;
+}
+
+int is_valid_move(move_t *moves, size_t s, coord_t sel[2]) {
 	int ret = 0;
 	for (size_t i=0;i<s;i++) {
-		if (cb[0].row==moves[i].start.row&&\
-			cb[0].col==moves[i].start.col&&\
-			cb[1].row==moves[i].end.row&&\
-			cb[1].col==moves[i].end.col) {
+		if (is_coord_equal(&moves[i].start,&sel[0])&&\
+			is_coord_equal(&moves[i].end,&sel[1])) {
 			ret=1;
 			goto cleanup;
 		}
@@ -251,50 +265,58 @@ cleanup:
 }
 
 int game_loop() {
-	game gm = {0};
-	textures tx = {0};
-	mv_vtable *mvt[NUM_PT] = {
-		moves_gen_p,moves_gen_n,moves_gen_b,
-		moves_gen_r,moves_gen_q,moves_gen_k
+	game_t game = {0};
+	texture_t textures = {0};
+	mvt_t *mvt[NUM_TYPES] = {
+		moves_p_gen,moves_n_gen,moves_b_gen,
+		moves_r_gen,moves_q_gen,moves_k_gen
 	};
 
 	InitWindow(WIDTH_BOARD, HEIGHT_BOARD, "chess");
 	SetTargetFPS(TARGET_FPS);
 
-	gm.fg=1;
-	gm.fm=0;
-	gm.turn=pc_W;
-	coord c = {0};
+	game.flag_g=1;
+	game.turn=c_W;
+	sel_clear(game.sel);
+	coord_t c={0};
 
-	board_init(&gm.bd);
-	textures_init(&tx);
+	board_init(&game.board);
+	textures_init(&textures);
+
 
 	while (!WindowShouldClose()) {
 	        BeginDrawing();
 	        ClearBackground(WHITE);
 
-	        board_draw(&gm.bd, &tx);
-	        if (gm.fg) {
-	        	clear_pm(gm.pm, &gm.pmc);
-	        	moves_gen_all(&gm.bd,gm.turn,gm.pm,&gm.pmc,
-	        		(mv_vtable**)mvt);
-	        	gm.fg=0;
-	        }
-		if (gm.fm)
-		        moves_draw(gm.pm, gm.pmc, &gm.cb[0]);
-		if (is_click(&gm.mp)) {
-			gm.fm=!gm.fm;
-			px_to_coord(&gm.mp,&c);
-			cb_push(gm.cb,&c);
-		}
-	        if (is_valid_move(gm.pm, gm.pmc, gm.cb)) {
-	        	move_make(&gm.bd, gm.cb);
-	        	gm.fg=1;
-	        }
+		int sel_empty=is_sel_empty(game.sel);
+		int sel_full=is_sel_full(game.sel);
+		int clicked=is_click(&game.mp, &c);
+		int piece_clicked=is_click_piece(&game.board,&c);
+		int valid_move=is_valid_move(game.pmoves,game.pmc,game.sel);
 
+	        board_draw(&game.board, &textures);
+	        if (game.flag_g) {
+	        	pmoves_clear(game.pmoves, &game.pmc);
+	        	moves_all_gen(&game.board,game.turn,game.pmoves,&game.pmc,(mvt_t**)mvt);
+	        	game.flag_g=0;
+	        }
+	        if (clicked)
+	        	sel_push(game.sel, &c);
+	        if (sel_full) {
+	        	if (valid_move) {
+	        		move_make(&game.board, game.sel);
+	        		game.flag_g=1;
+	        	}
+	        	sel_clear(game.sel);
+	        }
+	        if (!sel_empty&&!sel_full&&piece_clicked)
+	        	moves_draw(game.pmoves,game.pmc,&c);
+	        printf("empty=%d full=%d clicked=%d piece_clicked=%d valid_move=%d c=(%d,%d) sel[0]=(%d %d) sel[1]=(%d %d)\n",
+		        sel_empty, sel_full, clicked, piece_clicked, valid_move, c.row, c.col,
+		        game.sel[0].row,game.sel[0].col,game.sel[1].row,game.sel[1].col);
 	        EndDrawing();
 	}
-	textures_deinit(&tx);
+	textures_deinit(&textures);
 
 	CloseWindow();
 	return 0;
